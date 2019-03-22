3b0f34e56d2be6d9e65a7f81051148a5f6b5f59b
Nitza <rondon.nitza@gmail.com>
2019-03-22 11:50:10 -0400

    Project one files

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# ProjectOne
+# GroupProject1
+first group project
+###“How many of you get tired of your monotonous routine and feel like you need to get out for an evening, but you’re not exactly sure what you want to do? 
+* You want to break out of you comfort zone of sitting at your local dive bar you frequently visit - and go experience something new?”
+* The name of our project - EasyEvening
+* The common problem that people have when they want to have a fun night out - “what should we do tonight? Idk”
+* A lot of the time, even if we live in a city for years on end - we often find ourselves sitting on the couch around 5-6pm on a Friday or Saturday not sure what to entertain ourselves with for the evening
+
+###EasyEvening App
+*EasyEvening is a way to create a date night, get together with friends, or entertain people that are your guests
+*Our application uses the both the Zomato API and the TicketMaster API to retrieve the desired data so  our user is able to see the options of both highly rated restaurants and events going on a 25 mile radius based on the city and state they enter in the input fields
+
+###Tools Used
+Restuarant details from the Zomato API
+Event details from the Ticket master API 
+User validation using modals
+New library was mdbootstap for design
---
+++ assets/javascript/app.js
@@ -0,0 +1,181 @@
+$(document).ready(function () {
+
+    var queryLocationURL = "https://developers.zomato.com/api/v2.1/locations";
+
+    $("#sumbitCityState").on("click", function (event) {
+        $("#rest-deets").empty();
+        $("#event-deets").empty();
+        event.preventDefault();
+        // user input values
+        var userLocationInput = $("#city").val().trim();
+        var userStateInput = $("#state").val().trim();
+        var userCityState = userLocationInput + " " + userStateInput;
+
+        // input validation
+        var modal = $("#mymodal");
+        if (userLocationInput.length !== 0 &&
+            userStateInput !== "") {
+
+            // first ajax call
+            var queryURL = "https://app.ticketmaster.com/discovery/v2/events.json?size=10&apikey=ujQ6emhGAxGPdAKfj3Xpd11EGKJaqgdG";
+            $.ajax({
+                url: queryURL + "&city=" + userLocationInput,
+                method: "GET",
+                success: function (TMObj) {
+                    console.logTMObj;
+                }
+            }).then(function (TMObj) {
+                $.each(TMObj._embedded.events, function (index, value) {
+
+                    var $eventListItem = $("<li class='list-group-item event' id='" + value.name + "'>");
+                    $eventListItem.append($("<h5>" + value.classifications[0].genre.name + "</h5>"));
+                    var $eventList = $("<ul>");
+                    $eventList.addClass("list-group");
+                    $("#event-deets").append($eventList);
+                    $eventListItem.append("<h5 class = 'eventName'>" + value.name + "</h5>");
+                    $eventListItem.append("<h6>" + "Event Genre: " + value.classifications[0].genre.name + "</h6>");
+                    $eventListItem.append("<h6>" + "Location: " + value._embedded.venues[0].name + "</h6>");
+                    $eventListItem.append("<h6>" + "Event Date: " + value.dates.start.localDate + "<h6>");
+                    $eventListItem.append("<h6>" + "Start Time: " + moment(value.dates.start.localTime, 'HH:mm').format("hh:mm") + "<h6>");
+                    $eventListItem.append("<h6>" + "Address: " + value._embedded.venues[0].address.line1 + value._embedded.venues[0].city.name + "</h6>");
+                    $eventListItem.append("<h6>" + "<a target='_blank' href=" + value.url + ">" + "Click Here for tickets" + "</a>");
+                    $eventListItem.append("<i class='far fa-heart eheart' heart-state='empty' id='eventHeart_" + index + "'></i>");
+
+                    $eventList.append($eventListItem);
+                });
+                $(".eheart").on("click", function (event) {
+                    event.preventDefault();
+                    $("#insertFavEvent").empty();
+                    var state = $(this).attr("heart-state");
+
+                    if (state === "empty") {
+                        $(this).removeClass("far");
+                        $(this).addClass("fas");
+                        $(this).attr("heart-state", "full");
+                        var eventNameText = $(this).parent().find('.eventName').text();
+                        console.log($(this).parent().find('.eventName').text());
+                        console.log(this);
+                        console.log()
+                        $("#insertFavEvent").empty();
+                        $("#insertFavEvent").html(eventNameText);
+
+                        $(".eheart").hide();
+                        $(this).show();
+                    } else {
+                        $(this).addClass("far");
+                        $(this).removeClass("fas");
+                        $(this).attr("heart-state", "empty");
+                        $("#insertFavEvent").empty();
+                        $("#insertFavEvent").append("Click a heart to choose your event for your EasyEvening!");
+                        $(".eheart").show();
+                    };
+                });
+            });
+
+            $.ajax({
+                url: queryLocationURL + "?query=" + userCityState,
+                method: 'GET',
+                beforeSend: function (xhr) {
+                    xhr.setRequestHeader('Accept', 'application/json');
+                    xhr.setRequestHeader('user-key', '5bf4f29ca59c03031cc0830248eed6b3');
+                },
+                success: function (zomdata) {
+                }
+            }).then(function (zomdata) {
+                // location lat and long and entity_id
+                var objLocationEntityId = zomdata.location_suggestions[0].entity_id;
+                var objLocationLat = zomdata.location_suggestions[0].latitude;
+                var objLocationLon = zomdata.location_suggestions[0].longitude;
+
+                    // Zom search
+                    var radius = 5;
+                    var count = 10;
+                    var queryZomSearchURL = "https://developers.zomato.com/api/v2.1/search?";
+
+                    $.ajax({
+                        url: queryZomSearchURL + "entity_id=" + objLocationEntityId + "&entity_type=city" + "&count=" + count + "&lat=" + objLocationLat + "&lon=" + objLocationLon + "&radius=" + radius,
+                        method: 'GET',
+                        beforeSend: function (xhr) {
+                            xhr.setRequestHeader('Accept', 'application/json');
+                            xhr.setRequestHeader('user-key', '5bf4f29ca59c03031cc0830248eed6b3');
+                        },
+                        success: function (zomdata2) {
+                        }
+                    }).then(function (zomdata2) {
+                        // validation if there is data to show
+                        if (zomdata2.results_found > 0) {
+
+                            for (var i = 0; i < 10; i++) {
+
+                                var $restList = $("<ul>");
+                                $restList.addClass("list-group");
+
+                                // Add the newly created element to the DOM
+                                $("#rest-deets").append($restList);
+                                var restName = zomdata2.restaurants[i].restaurant.name;
+                                var $restListItem = $("<li class='list-group-item restaurant' id=" + restName + ">");
+                                $restListItem.append($("<h5 class='restName'>" + restName + "</h5>"));
+                                $restListItem.append("<h6>" + zomdata2.restaurants[i].restaurant.location.address);
+                                $restListItem.append("<h6>" + "Locality: " + zomdata2.restaurants[i].restaurant.location.locality);
+                                $restListItem.append("<h6>" + "Cuisine: " + zomdata2.restaurants[i].restaurant.cuisines);
+                                $restListItem.append("<h6>" + "Rating text: " + zomdata2.restaurants[i].restaurant.user_rating.rating_text);
+                                $restListItem.append("<h6>" + "Rating number: " + zomdata2.restaurants[i].restaurant.user_rating.aggregate_rating);
+                                $restListItem.append("<h6>" + "<a target='_blank' href=" + zomdata2.restaurants[i].restaurant.url + ">" + "Zomato MENU URL" + "</a>");
+                                $restListItem.append("<h6>" + "<a target='_blank' href=" + zomdata2.restaurants[i].restaurant.photos_url + ">" + "Zomato PHOTOS URL" + "</a>");
+                                $restListItem.append("<i class='far fa-heart heart' heart-state='empty' id='heart_" + i + "'></i>");
+                                $restList.append($restListItem);
+                            };
+
+                            // if there is no data to show
+                        } else {
+                            modal.show();
+                            $(".errormessage").text("No data to show");
+                        }
+                        $(".heart").on("click", function (event) {
+                            event.preventDefault();
+                            $("#insertFavRest").empty();
+                            var state = $(this).attr("heart-state");
+
+                            if (state === "empty") {
+                                $(this).removeClass("far");
+                                $(this).addClass("fas");
+                                $(this).attr("heart-state", "full");
+                                var restNameText = $(this).parent().find('.restName').text();
+                                console.log($(this).parent().find('.restName').text());
+                                console.log(this);
+                                $("#insertFavRest").empty();
+                                $("#insertFavRest").html(restNameText);
+                                $(".heart").hide();
+                                $(this).show();
+                            } else {
+                                $(this).addClass("far");
+                                $(this).removeClass("fas");
+                                $(this).attr("heart-state", "empty");
+                                $("#insertFavRest").empty();
+                                $("#insertFavRest").append("Click a heart to choose your restaurant for your EasyEvening!");
+                                $(".heart").show();
+
+                            }
+                        })
+
+                    })
+            });
+
+            // if input fields empty
+        } else
+            if (userLocationInput.length === 0) {
+                modal.show();
+                $(".errormessage").text("Please enter a city");
+            }
+            else
+                if (userStateInput === "") {
+                    modal.show();
+                    $(".errormessage").text("Please select the state");
+                };
+
+        // modal closing
+        $(".close").on("click", function () {
+            modal.hide();
+        });
+    });
+});
